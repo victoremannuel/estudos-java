f954ad1cdf40f294d41959b301b64029fdf2c60e
victor <vic.emannuel@gmail.com>
2024-02-06 17:36:46 -0300

    collections

---
+++ estudos/src/main/java/br/com/victor/j0012Collections/QualColecaoUsar.java
@@ -0,0 +1,13 @@
+package br.com.victor.j0012Collections;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class QualColecaoUsar {
+	
+	public static void main(String[] args) {
+		
+		Collection<Aluno> alunos = new HashSet<>();
+		
+	}
+}
